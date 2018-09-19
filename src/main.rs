aeeb9532fef05d5aabcffbaff82d5907d65ee876
Voronchikhin <m.voronchikhin@g.nsu.ru>
2018-09-19 13:02:41 +0700

    try fix AddrInUse

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,38 @@
+extern crate nix;
 use std::net::*;
-use std::thread::*;
 use std::{env,str};
+use std::str::FromStr;
 use std::collections::HashMap;
-use std::thread;
 use std::time::*;
+use std::os::unix::io::AsRawFd;
+use std::{io, mem};
+use std::thread::*;
+use std::thread;
+use nix::sys::socket;
+use nix::sys::socket::sockopt::ReuseAddr;
 
 fn connection_listener(){
     let mut cache : HashMap<SocketAddr, std::time::SystemTime> = HashMap::new();
     let args: Vec<String> = env::args().collect();
-    let socket = UdpSocket::bind("0.0.0.0:6000").expect("could not bind socket");
+    let socket = UdpSocket::bind("127.0.0.1:6000").expect("could not bind socket");
+
+    socket.set_multicast_loop_v4(true).expect("could not use multicast");
     println!("{:?}",socket);
-    let mcast_group: IpAddr =  args[1].parse().expect("wrong address");
+    let mcast_group: IpAddr =  match args.get(1){
+        Some(str) => str.parse(),
+        None => "239.0.0.1".parse(),
+    }.expect("wrong address");
     match mcast_group {
         IpAddr::V4(ipv4) => socket.join_multicast_v4(&ipv4,&Ipv4Addr::new(0,0,0,0)),
         IpAddr::V6(ipv6) => socket.join_multicast_v6(&ipv6,0),
     };
     let mut buffer = [0u8; 1600];
-    socket.set_read_timeout(Option::from(Duration::from_secs(3)));
+    socket::setsockopt(socket.as_raw_fd(), ReuseAddr, &true).expect("setsockopt failed");
+    //socket.set_read_timeout(Option::from(Duration::from_secs(3)));
     loop {
         let (_, src_addr) = socket.recv_from(&mut buffer).expect("failed to read from server");
         cache.entry(src_addr).or_insert(std::time::SystemTime::now());
-        println!("{}",cache.iter().filter(|(_,x)| { x.elapsed().unwrap()<Duration::from_secs(2) }).count());
+        println!("{}",cache.iter().filter(|(_,x)| { x.elapsed().unwrap()<Duration::from_secs(4) }).count());
 
         println!("{}",str::from_utf8(&mut buffer).unwrap());
     }
@@ -35,16 +47,18 @@ fn sender(){
     let socket = UdpSocket::bind((any,0))
         .expect("could not create server ");
     loop {
-        socket.send_to("Hello world!".as_bytes(),&(mcast_group, port)).expect("");
+        match socket.send_to("Hello world!".as_bytes(),&(mcast_group, port)){
+            Ok(size) => println!("send {}bytes", size),
+            Err(e)   => println!("sending error"),
+        }
         thread::sleep(Duration::from_millis(1000));
-        println!("send successfully");
     }
 }
 
 fn main() {
-    let _listener_handle = thread::Builder::new().spawn(connection_listener);
+    let _listener_handle = thread::Builder::new().name("listener".into()).spawn(connection_listener);
     thread::sleep(Duration::from_millis(100));
-    let _sender_handle = thread::Builder::new().spawn(sender);
+    let _sender_handle = thread::Builder::new().name("sender".into()).spawn(sender);
     _sender_handle.unwrap().join();
     _listener_handle.unwrap().join();
 }
